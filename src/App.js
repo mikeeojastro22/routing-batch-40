3112e413fa843f5da40ab3b18c7f116cb4f4a05f
Mikee Ojastro <mikeeojastro@gmail.com>
2025-07-03 20:06:15 +0800

    July 3, 2025

--- src/App.js
+++ src/App.js
@@ -9,14 +9,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
       {/* A <BrowserRouter></BrowserRouter> stores the current location in the browser's address bar using clean URLs and navigates the browser's built-in history stack */}
-       
+        <Routes>
           {/* Rendered anywhere in the application. <Routes></Routes> will match a set of child routes from the current location */}
           {/* Route - most important part of react-router. They couple URL segments to components/pages, data loading and data mutations. Through route nesting, complex layouts and data dependencies become simple and declarative */}
-
-          {/* index is similar to root (/) */}
-            
+          <Route path="/" element={<Navigation />}>
+            {/* index is similar to root (/) */}
+            <Route index element={<Home />} />
+            <Route path="bank" element={<Bank />} />
+            <Route path="budget" element={<Budget />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>  
     </div>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,9 +7,13 @@ function Navigation() {
             <header>
                 <nav>
                     {/* NavLink is a special kind of <Link> that knows where or not it is "active" or "pending". This is useful when building a navigation menu. */}
+                    <NavLink className="item" to="/">Home</NavLink>
+                    <NavLink className="item" to="bank">Bank</NavLink>
+                    <NavLink className="item" to="budget">Budget</NavLink>
                 </nav>
             </header>
             {/* Outlet should be the parent route of elements to render their child route elements */}
+            <Outlet />
         </div>
     );
 }
