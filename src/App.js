c06a9f5f6c3fc4e1878a61c4402437560aeb2657
Mikee Ojastro <mikeeojastro@gmail.com>
2025-07-03 18:46:13 +0800

    July 3, 2025

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Navigation from './components/Navigation/Navigation.jsx';
+import Home from './pages/Home/Home.jsx';
+import Bank from './pages/Bank/Bank.jsx';
+import Budget from './pages/Budget/Budget.jsx';
+import NotFound from './pages/NotFound/NotFound.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* A <BrowserRouter></BrowserRouter> stores the current location in the browser's address bar using clean URLs and navigates the browser's built-in history stack */}
+       
+          {/* Rendered anywhere in the application. <Routes></Routes> will match a set of child routes from the current location */}
+          {/* Route - most important part of react-router. They couple URL segments to components/pages, data loading and data mutations. Through route nesting, complex layouts and data dependencies become simple and declarative */}
+
+          {/* index is similar to root (/) */}
+            
+    </div>
+  );
+}
+
+export default App;
